79602711bfba486ff5917bc43f05069e2c2930e2
rramirez1401 <raul.ramirez1401+1@gmail.com>
2025-08-08 02:57:59 +0000

    Hito 3, requerimiento 1 completado: productos agregados en backoffice

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Flan
 
 # Register your models here.
+
+@admin.register(Flan)
+class ProductoAdmin(admin.ModelAdmin):
+    pass
--- web/models.py
+++ web/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Flan(models.Model):
+    flan_uuid = models.UUIDField("UUID", default=uuid.uuid4, editable=False)
+    name = models.CharField("Nombre", max_length=64)
+    description = models.TextField("Descripcion")
+    price = models.DecimalField("Precio", max_digits=5, decimal_places=0)
+    imagen = models.ImageField("Imagen", upload_to="productos/")
+    slug = models.SlugField("Slug", unique=True, blank=True, editable=False)
+    is_private = models.BooleanField("Privado", default=False)
+
+## Generar automaticamente el slug a partir del nombre
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            count = 1
+            while Flan.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{count}"
+                count += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
